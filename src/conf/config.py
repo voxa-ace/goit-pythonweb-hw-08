3f4de190d28b65e3fce10fc476bca8d3b3c8d574
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2024-11-28 21:42:19 +0200

    fix import

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,9 +1,9 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    database_url: str  
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  
 
 settings = Settings()
