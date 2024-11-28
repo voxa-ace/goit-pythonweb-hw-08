67aaa336217aaf4aba117241c2c8e2e094c0b0b7
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2024-11-28 22:26:21 +0200

    commit

--- src/api/birthdays.py
+++ src/api/birthdays.py
@@ -4,10 +4,11 @@ from src.services.birthday_service import get_upcoming_birthdays
 from src.schemas import ContactResponse
 from src.database.db import get_db
 
-# Create router
 router = APIRouter()
 
-@router.get("/birthdays/", response_model=list[ContactResponse])
+@router.get("/", response_model=list[ContactResponse])
 def get_birthdays(days_range: int = 7, db: Session = Depends(get_db)):
-    """Get contacts with upcoming birthdays within the specified range"""
+    """
+    Get contacts with upcoming birthdays within the specified range of days.
+    """
     return get_upcoming_birthdays(db, days_range)
